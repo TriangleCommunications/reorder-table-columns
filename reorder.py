3bd883af07bcb2ab74ca61fa1548da30728d6050
Jason Taylor <jason@infinitebubble.com>
2022-02-04 21:56:05 -0500

    include schema when finding foreign keys

--- reorder.py
+++ reorder.py
@@ -177,9 +177,10 @@ def get_foreign_keys(database: str, schema: str, table: str, user: str, password
                 JOIN information_schema.key_column_usage kcu ON tc.constraint_name = kcu.constraint_name
                 JOIN information_schema.constraint_column_usage ccu ON ccu.constraint_name = tc.constraint_name
                 WHERE constraint_type = 'FOREIGN KEY'
+                AND ccu.table_schema = %s
                 AND ccu.table_name = %s
             """,
-                (table,),
+                (schema,table,),
             )
             return [dict(row) for row in curs.fetchall()]
 
